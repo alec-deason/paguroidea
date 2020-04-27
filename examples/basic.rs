0b729c08c60385472b32074c3939adb441dd0ce7
Alec Deason <alec@tinycountry.com>
2020-04-27 11:21:12 -0700

    Adjust example pattern so it works with vanilla SuperDirt samples

--- examples/basic.rs
+++ examples/basic.rs
@@ -16,7 +16,7 @@ fn main() {
     }
     let player = sound::Player::new(samples);
 
-    let pattern = sound(mini_notation::parse_pattern("<hi:1 hi*2> <~ hi:3 lo:2*2 hi*2> <~ lo:3*2 hi> <lo:1 lo:3*2>"));
+    let pattern = sound(mini_notation::parse_pattern("<tabla:1 tabla*2> <~ tabla:3 drum:2*2 tabla*2> <~ drum:3*2 tabla> <drum:1 drum:3*2>"));
     let pattern = jux_by(unit(1.0), |p| off(unit((1,4).into()), |p| sometimes_by(unit(0.75), |p| chunk(2, |p| rev(p.clone()), p), p.clone()), p), pattern);
     player.set_pattern("d1", pattern);
     player.start_playback();
